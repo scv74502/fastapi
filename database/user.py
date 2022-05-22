b23dd7a4bcb972589724da3fe2b2c7a90b172815
kunwoo park <scv74502@gmail.com>
2022-05-23 05:19:03 +0900

    server query fixed

--- database/user.py
+++ database/user.py
@@ -1,4 +1,5 @@
 from .db import dynamodb
+from typing import Optional
 from botocore.exceptions import ClientError
 from fastapi.responses import JSONResponse
 from boto3.dynamodb.conditions import Key
@@ -52,18 +53,36 @@ def delete_user(user: dict):
 
 def update_user(user: dict):
     try:
+        UpdateExpression = "SET "
+        ExpressionAttributeValues = {}
+
+        if user["position_score"] is not None:
+            UpdateExpression += "position_score = :position_score,"
+            ExpressionAttributeValues[":position_score"] = user["position_score"]
+
+        if user["previous_project"] is not None:
+            UpdateExpression += "previous_project = :previous_project,"
+            ExpressionAttributeValues[":previous_project"] = user["previous_project"]
+
+        if user["tech_stack"] is not None:
+            UpdateExpression += "tech_stack = :tech_stack,"
+            ExpressionAttributeValues[":tech_stack"] = user["tech_stack"]
+
+        UpdateExpression = UpdateExpression.rstrip(",")
+
         response = table.update_item(
             Key={
                 "user_id": user["user_id"]
             },
-            UpdateExpression="SET position_score = :position_score, previous_project = :previous_project, tech_stack = :tech_stack",
-            ExpressionAttributeValues={
-                ":position_score": user["position_score"],
-                ":previous_project": user["previous_project"],
-                ":tech_stack": user["tech_stack"]
-            }
+            UpdateExpression = UpdateExpression,
+            # UpdateExpression="SET position_score = :position_score, previous_project = :previous_project, tech_stack = :tech_stack",
+            # ExpressionAttributeValues={
+                # ":position_score": user["position_score"],
+                # ":previous_project": user["previous_project"],
+                # ":tech_stack": user["tech_stack"]
+            # }
+            ExpressionAttributeValues = ExpressionAttributeValues
         )
         return response
     except ClientError as e:
-        print(e)
         return JSONResponse(content=e.response["Error"], status_code=500)
